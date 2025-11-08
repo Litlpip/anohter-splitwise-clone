13fc3dad08f91bf6d2d84009d1665b05a78129d8
rdbukharov <rdbukharov@avito.ru>
2025-11-08 21:05:54 +0300

    pwa is working

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pg5vc745hi"
+    "revision": "0.m57fnni5dsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
